f4a20701481ad4e88207d6a8a6216f778c5a259f
mgrinko <glukym@gmail.com>
2019-02-13 09:46:33 +0200

    MEnu template was added

--- menu.js
+++ menu.js
@@ -1,13 +1,25 @@
-export default class Menu {
-  constructor({ element }) {
-    this._element = element;
+import Component from './component.js';
+
+export default class Menu extends Component {
+  constructor({ element, title = '12312', items = [1, 2, 3] }) {
+    super({ element });
+
+    this._title = title;
+    this._items = items;
 
     this._render();
   }
 
   _render() {
     this._element.innerHTML = `
-      
+      <h2 class="menu__title">${ this._title }</h2>
+      <ul class="menu__items-list">
+        ${ this._items.map(item => `
+  
+          <li class="menu__item">${ item }</li>
+  
+        `).join('') }
+      </ul>
     `;
   }
 }
